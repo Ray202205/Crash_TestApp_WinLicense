82641d7058d3ab6a385b12fb6c5a4167591393e1
aGrainOfSand <52094473+Ray202205@users.noreply.github.com>
2022-05-28 12:29:03 +0800

    Add files via upload

---
+++ TestApp_WinLicense/TestApp_WinLicense/TestApp_WinLicense.h
@@ -0,0 +1,32 @@
+﻿
+// TestApp_WinLicense.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTestAppWinLicenseApp:
+// 有关此类的实现，请参阅 TestApp_WinLicense.cpp
+//
+
+class CTestAppWinLicenseApp : public CWinApp
+{
+public:
+	CTestAppWinLicenseApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestAppWinLicenseApp theApp;
---
+++ TestApp_WinLicense/TestApp_WinLicense/TestApp_WinLicenseDlg.h
@@ -0,0 +1,35 @@
+﻿
+// TestApp_WinLicenseDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CTestAppWinLicenseDlg 对话框
+class CTestAppWinLicenseDlg : public CDialogEx
+{
+// 构造
+public:
+	CTestAppWinLicenseDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTAPP_WINLICENSE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg void OnBnClickedOk();
+	DECLARE_MESSAGE_MAP()
+};
